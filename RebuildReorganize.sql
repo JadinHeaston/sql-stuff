74400c36ac2a80a456d64ac53ef6a4f6ae8a9eca
Jadin Heaston <jadin.heaston@como.gov>
2023-05-31 13:52:42 -0500

    Adding size of index to index selects.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- RebuildReorganize.sql
+++ RebuildReorganize.sql
@@ -32,6 +32,7 @@ INSERT INTO @tempIndexTable (
             o.[name],
             s.[avg_fragmentation_in_percent],
             o.[type_desc],
+            (CAST(s.page_count as float) * CAST(8 as float)) / CAST(1000 as float) as index_size_mb,
             CASE
                 WHEN s.[avg_fragmentation_in_percent] > 10
                 AND s.[avg_fragmentation_in_percent] < 30 THEN 'REORGANIZE'
@@ -107,6 +108,7 @@ INSERT INTO @tempIndexTable (
             o.[name],
             s.[avg_fragmentation_in_percent],
             o.[type_desc],
+            (CAST(s.page_count as float) * CAST(8 as float)) / CAST(1000 as float) as index_size_mb,
             CASE
                 WHEN s.[avg_fragmentation_in_percent] > 10
                 AND s.[avg_fragmentation_in_percent] < 30 THEN 'REORGANIZE'
